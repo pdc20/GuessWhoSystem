68a5d4609a439578673f52f7197faf8e80e97772
pdc20 <kookie2332@gmail.com>
2021-12-05 03:03:19 +1100

    Refined checker.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GuessWhoSystem
+A GUI system to help with *Guess Who?* tournaments.
+
+The system takes data from a Google spreadsheet which records registrations from players wanting to join the tournament.
+The data can then be manipulated and displayed in a readable manner as the tournament is running.
+
+The system uses the PyQt5 GUI framework which means that the system is cross-platform. It also uses the Google Sheets 
+API for Python.
--- main.py
+++ main.py
@@ -86,16 +86,17 @@ class Player:
     """Class containing data on a player."""
 
     class PlayerLogEntry:
-        def __init__(self, bracketRound, guesser, guessedPlayer, guessedIdentity, isGuessCorrect):
-            self.bracketRound = bracketRound
+        def __init__(self, guesser, guessedPlayer, guessedIdentityString):
             self.guesser = guesser
             self.guessedPlayer = guessedPlayer
-            self.guessedIdentity = guessedIdentity
-            self.isGuessCorrect = isGuessCorrect
-            self.verdict = "MATCH" if isGuessCorrect else "INCORRECT"
+            self.guessedIdentity = guessedIdentityString
 
         def __str__(self):
-            return f"On round {self.bracketRound}, player {self.guesser.nickname} guessed {self.guessedPlayer.nickname} was {self.guessedIdentity.handle}. Result: {self.verdict}"
+            return f"Player {self.guesser.nickname} guessed {self.guessedPlayer.nickname} was " \
+                   f"{self.guessedIdentity}. Result: {(lambda x: 'MATCH' if x else 'INCORRECT')(self.getVerdict())}"
+
+        def getVerdict(self):
+            return True if self.guessedPlayer.handle == self.guessedIdentity else False
 
     def __init__(self, id, handle, nickname, character, hostStatus, remarks):
         self.id = id
@@ -108,6 +109,13 @@ class Player:
         self.lives = 2
         self.playHistory = []
 
+    def addGuess(self, guessedPlayer, guessedIdentityString):
+        entry = Player.PlayerLogEntry(self.nickname, guessedPlayer, guessedIdentityString)
+        self.playHistory.append(entry)
+
+        # If we have a wrong guess, then we decrease the number of lives.
+        if not entry.getVerdict(): self.lives -= 1
+        else: guessedPlayer.lives = 0 # Correct guess means opponent is out.
 
 class MainApp(QApplication):
     def __init__(self, debug=False):
@@ -118,14 +126,60 @@ class MainApp(QApplication):
         self.displayWindow.show()
         self.backendWindow.show()
 
+        # Event handlers.
         self.backendWindow.dataUpdateButton.clicked.connect(self.dataUpdateButtonClicked)
         self.backendWindow.nicknameCombo.activated.connect(self.nicknameComboActivated)
         self.displayWindow.checkButton.clicked.connect(self.checkButtonClicked)
         self.backendWindow.credentialsBrowseButton.clicked.connect(self.credentialsBrowseButtonClicked)
         self.backendWindow.streamDirectoryBrowseButton.clicked.connect(self.streamDirectoryBrowseButtonClicked)
+        self.displayWindow.recordGuessButton.clicked.connect(self.recordGuessButtonClicked)
+        self.displayWindow.checkerClearButton.clicked.connect(self.checkerClearButtonClicked)
 
+        self.displayWindow.guesserCombo.currentIndexChanged.connect(self.checkerComboChanged)
+        self.displayWindow.checkerNicknameCombo.currentIndexChanged.connect(self.checkerComboChanged)
+        self.displayWindow.checkerPlayerNameCombo.currentIndexChanged.connect(self.checkerComboChanged)
+
+        # Other things to do
+        self.displayWindow.recordGuessButton.setEnabled(False)
+        self.displayWindow.checkButton.setEnabled(False)
+        self.displayWindow.checkerVerdictLabel.setText("...")
+
+        # Debug operations
         if debug:
             self.performDebugOperations()
+
+    def checkerComboChanged(self):
+        if not self.displayWindow.areComboBoxesEmpty():
+            self.displayWindow.checkButton.setEnabled(True)
+        else:
+            self.displayWindow.checkButton.setEnabled(False)
+
+    def checkerClearButtonClicked(self):
+        # Clear all comboboxes
+        self.displayWindow.guesserCombo.setCurrentIndex(0)
+        self.displayWindow.checkerNicknameCombo.setCurrentIndex(0)
+        self.displayWindow.checkerPlayerNameCombo.setCurrentIndex(0)
+
+        # Enable all comboboxes.
+        self.displayWindow.guesserCombo.setEnabled(True)
+        self.displayWindow.checkerNicknameCombo.setEnabled(True)
+        self.displayWindow.checkerPlayerNameCombo.setEnabled(True)
+
+        # Disable record guess and check buttons
+        self.displayWindow.recordGuessButton.setEnabled(False)
+        self.displayWindow.checkButton.setEnabled(False)
+
+        self.displayWindow.checkerVerdictLabel.setText("...")
+
+    def recordGuessButtonClicked(self):
+        # Compile guess into player log.
+        guesserIdx = self.displayWindow.guesserCombo.currentIndex() - 1
+        checkerNicknameIdx = self.displayWindow.checkerNicknameCombo.currentIndex() - 1
+        handle = self.displayWindow.checkerPlayerNameCombo.currentText()
+        self.players[guesserIdx].addGuess(self.players[checkerNicknameIdx], handle)
+
+        # Disable the button to prevent double-writing.
+        self.displayWindow.recordGuessButton.setEnabled(False)
 
     def credentialsBrowseButtonClicked(self):
         directory = str(QFileDialog.getExistingDirectory(None, "Select Folder"))
@@ -137,11 +191,22 @@ class MainApp(QApplication):
 
     def checkButtonClicked(self):
         # First ensure that all the comboboxes are not on index 0 (Select...)
-        if self.displayWindow.areComboBoxesEmpty(): return
+        if self.displayWindow.areComboBoxesEmpty():
+            # Ensure that the record guess button is disabled.
+            self.displayWindow.recordGuessButton.setEnabled(False)
+            return
         # Check whether the guess is correct
         if self.players[self.displayWindow.checkerNicknameCombo.currentIndex() - 1].handle == self.displayWindow.checkerPlayerNameCombo.currentText():
             self.displayWindow.checkerVerdictLabel.setText("MATCH!")
         else: self.displayWindow.checkerVerdictLabel.setText("INCORRECT")
+
+        # Disable all comboboxes
+        self.displayWindow.guesserCombo.setEnabled(False)
+        self.displayWindow.checkerNicknameCombo.setEnabled(False)
+        self.displayWindow.checkerPlayerNameCombo.setEnabled(False)
+
+        # Allow guess to be recorded.
+        self.displayWindow.recordGuessButton.setEnabled(True)
 
     def nicknameComboActivated(self):
         index = self.backendWindow.nicknameCombo.currentIndex()
